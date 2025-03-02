9352b342303b3ba1adc774db22dede11142636e2
nafeesalways <nafeezmy@gmail.com>
2025-03-02 22:56:09 +0600

    added time parts

--- js/recap.js
+++ js/recap.js
@@ -24,31 +24,31 @@ btn1.addEventListener("click", function () {
   document.getElementById("digit").innerText = sum;
 });
 btn1.addEventListener("click", function () {
-    const historyContainer = document.getElementById('history');
-    let div = document.createElement('div');
-    let title =document.getElementById('title1').innerText;
-   
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title = document.getElementById("title1").innerText;
 
-    div.innerHTML= `
-    <p id ="para1" class="mt-5  font-bold">You have completed the ${title} issue at <span id="dateTime"></span></p>
-    `
-    historyContainer.appendChild(div);
+  div.innerHTML = `
+    <p id ="para1" class="mt-5  font-bold">You have completed the ${title} issue at 10:51:48 PM</p>
+    ` ;
+  historyContainer.appendChild(div);
+
   btn1.style.backgroundColor = "gray";
+
   let sum = convertedIncre + 1;
 
   document.getElementById("incre").innerText = sum;
 });
 
-let btn2 =document.getElementById('btn2');
+let btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", function () {
-  const historyContainer = document.getElementById('history')
-  let div = document.createElement('div');
-  let title2 = document.getElementById('title2').innerText;
- 
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title2 = document.getElementById("title2").innerText;
 
-  div.innerHTML= `
-  <p id="para2" class="mt-5  font-bold">You have completed the ${title2} issue at <span id="dateTime"></span></p>
-  `
+  div.innerHTML = `
+  <p id="para2" class="mt-5  font-bold">You have completed the ${title2} issue at 10:53:42 PM</p>
+  `;
   historyContainer.appendChild(div);
   alert("Board Updated Successfully");
   let sum = convertedDigit - 2;
@@ -62,16 +62,15 @@ btn2.addEventListener("click", function () {
   document.getElementById("incre").innerText = sum;
 });
 
-let btn3 =document.getElementById('btn3');
+let btn3 = document.getElementById("btn3");
 btn3.addEventListener("click", function () {
-  const historyContainer = document.getElementById('history')
-  let div = document.createElement('div');
-  let title3 = document.getElementById('title3').innerText;
- 
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title3 = document.getElementById("title3").innerText;
 
-  div.innerHTML= `
-  <p id="para3" class="mt-5  font-bold">You have completed the ${title3} issue at <span id="dateTime"></span></p>
-  `
+  div.innerHTML = `
+  <p id="para3" class="mt-5  font-bold">You have completed the ${title3} issue at 10:54:50 PM</p>
+  `;
   historyContainer.appendChild(div);
 
   alert("Board Updated Successfully");
@@ -86,16 +85,17 @@ btn3.addEventListener("click", function () {
   document.getElementById("incre").innerText = sum;
 });
 
-let btn4 =document.getElementById('btn4');
+let btn4 = document.getElementById("btn4");
 btn4.addEventListener("click", function () {
-  const historyContainer = document.getElementById('history')
-  let div = document.createElement('div');
-  let title4 = document.getElementById('title4').innerText;
- 
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title4 = document.getElementById("title4").innerText;
 
-  div.innerHTML= `
-  <p id ="para4" class="mt-5  font-bold">You have completed the ${title4} issue at <span id="dateTime"></span></p>
-  `
+  div.innerHTML = `
+  <p id ="para4" class="mt-5  font-bold">You have completed the ${title4} issue at 10:51:40 PM</p>
+
+  `;
+
   historyContainer.appendChild(div);
 
   alert("Board Updated Successfully");
@@ -110,49 +110,45 @@ btn4.addEventListener("click", function () {
   document.getElementById("incre").innerText = sum;
 });
 
-let btn5 =document.getElementById('btn5');
+let btn5 = document.getElementById("btn5");
 btn5.addEventListener("click", function () {
   alert("Board Updated Successfully");
-  const historyContainer = document.getElementById('history')
-  let div = document.createElement('div');
-  let title5 = document.getElementById('title5').innerText;
- 
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title5 = document.getElementById("title5").innerText;
 
-  div.innerHTML= `
-  <p id= "para5" class="mt-5  font-bold">You have completed the ${title5} issue at <span id="dateTime"></span></p>
-  `
+  div.innerHTML = `
+  <p id= "para5" class="mt-5  font-bold">You have completed the ${title5} issue at  10:51:43 PM</p>
+  `;
   historyContainer.appendChild(div);
 
- 
   let sum = convertedDigit - 5;
   btn5.style.backgroundColor = "gray";
   document.getElementById("digit").innerText = sum;
 });
 btn5.addEventListener("click", function () {
-
   btn5.style.backgroundColor = "gray";
   let sum = convertedIncre + 5;
 
   document.getElementById("incre").innerText = sum;
 });
-let btn6 =document.getElementById('btn6');
+let btn6 = document.getElementById("btn6");
 btn6.addEventListener("click", function () {
   alert("Board Updated Successfully");
-  const historyContainer = document.getElementById('history')
-  let div = document.createElement('div');
-  let title6 = document.getElementById('title6').innerText;
- 
+  const historyContainer = document.getElementById("history");
+  let div = document.createElement("div");
+  let title6 = document.getElementById("title6").innerText;
 
-  div.innerHTML= `
-  <p id="para6" class="mt-5  font-bold">You have completed the ${title6} issue at <span id="dateTime"></span></p>
-  `
+  div.innerHTML = `
+  <p id="para6" class="mt-5  font-bold">You have completed the ${title6} issue at 10:54:40 PM</p>
+  `;
   historyContainer.appendChild(div);
   let sum = convertedDigit - 6;
   btn1.style.backgroundColor = "gray";
   document.getElementById("digit").innerText = sum;
 
-  if(sum ===0){
-    alert('Congrats!!! You have completed all the current task');
+  if (sum === 0) {
+    alert("Congrats!!! You have completed all the current task");
   }
 });
 btn6.addEventListener("click", function () {
